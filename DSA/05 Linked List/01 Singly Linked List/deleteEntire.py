897287b413d9c104b61bb4e122cae942f26d251d
iamgauravshukla <gauravshukla0008@gmail.com>
2021-10-16 01:14:37 +0530

    Added practice sets

--- DSA/05 Linked List/01 Singly Linked List/deleteEntire.py
+++ DSA/05 Linked List/01 Singly Linked List/deleteEntire.py
@@ -47,37 +47,15 @@ class SinglyLinked:
             while node is not None:
                 print(node.value)
                 node = node.next
-    #Deletion
-    def delNode(self, location):
+
+    # Delete Entire
+
+    def delEntireList(self):
         if self.head is None:
             print("The singly linked list doesn't exist")
         else:
-            if location == 0:
-                if self.head == self.tail:
-                    self.head = None
-                    self.tail = None
-                else:
-                    self.head = self.head.next
-            elif location == 1:
-                if self.head == self.tail:
-                    self.head = None
-                    self.tail = None
-                else:
-                    node = self.head
-                    while node is not None:
-                        if node.next == self.tail:
-                            break
-                        node = node.next
-                    node.next = None
-                    self.tail = node
-            else:
-                tempNode = self.head
-                index = 0
-                while index < location - 1:
-                    tempNode = tempNode.next
-                    index += 1
-                nextNode = tempNode.next
-                tempNode.next = nextNode.next
+            self.head = None
+            self.tail = None
 
 
 SinglyLinkedList = SinglyLinked()
@@ -87,7 +65,7 @@ SinglyLinkedList.insertSL(3, 0)
 SinglyLinkedList.insertSL(4, 2)
 
 print([node.value for node in SinglyLinkedList])
-SinglyLinkedList.delNode(0 )
+SinglyLinkedList.delEntireList()
 print([node.value for node in SinglyLinkedList])
 
 
